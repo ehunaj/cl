446a45737214778d657e25a2f35396206713e78b
ehunaj <56645834+ehunaj@users.noreply.github.com>
2020-01-24 16:51:57 +0800

    Update Classclient.py

--- Classclient.py
+++ Classclient.py
@@ -1,7 +1,14 @@
-# -*- coding: utf-8 -*-
-# Support ===ZNF TEAM BOTZ===
-from Dit.linepy import *
-from akad.ttypes import *
+# -*- coding: utf-8 -*
+from linepy import *
+from akad.ttypes import Message
+from datetime import datetime
+from time import sleep
+from bs4 import BeautifulSoup
+from humanfriendly import format_timespan, format_size, format_number, format_length
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse
+from gtts import gTTS
+from googletrans import Translator
+
 import time, asyncio, json, threading, codecs, sys, os, re, urllib, requests,subprocess,traceback,random
 from Naked.toolshed.shell import execute_js 
 from bs4 import BeautifulSoup
